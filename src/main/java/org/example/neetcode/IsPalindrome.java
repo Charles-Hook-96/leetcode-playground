fe70e4186847e6bfc23c3f02beb18ab9da80eff7
Charles Hook <charles.hook96@outlook.com>
2026-01-28 09:04:53 -0500

    new problems and sorts

---
+++ src/main/java/org/example/neetcode/IsPalindrome.java
@@ -0,0 +1,29 @@
+package org.example.neetcode;
+
+public class IsPalindrome {
+
+    public static boolean example(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (right > left && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
